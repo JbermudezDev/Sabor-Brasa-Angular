99679b6c241a3ba4984b63cf833e496caf1f753e
paula2704 <95259154+paula2704@users.noreply.github.com>
2025-04-10 09:39:59 -0500

    Botones y login

--- Sabor-Brasa-Angular/src/app/components/login/login.component.ts
+++ Sabor-Brasa-Angular/src/app/components/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth-service.service';
-
+import { ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent {
   email: string = '';
